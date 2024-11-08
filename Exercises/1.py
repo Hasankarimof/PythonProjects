162641fee7daac3a3569558354376a880d28d3d5
Hasankarimof <abdukarimovhasan4@gmail.com>
2024-11-08 12:37:17 +0100

    updated

---
+++ Exercises/1.py
@@ -0,0 +1,23 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+#
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+
